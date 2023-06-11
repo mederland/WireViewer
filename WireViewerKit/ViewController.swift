31163dad57c2efeb217116fb6e0e5799cd4cc918
mederland <mederland@gmail.com>
2023-06-11 17:36:24 -0500

    Initial Commit

---
+++ WireViewerKit/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  WireViewerKit
+//
+//  Created by Meder iZimov on 6/11/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
