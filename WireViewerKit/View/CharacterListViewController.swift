932552e3fa9f3af326fad52cabbce669961623ce
mederland <mederland@gmail.com>
2023-06-12 03:29:36 -0500

    Title

--- WireViewerKit/View/CharacterListViewController.swift
+++ WireViewerKit/View/CharacterListViewController.swift
@@ -7,86 +7,71 @@
 
 import UIKit
 
-class CharacterListViewController: UIViewController, UISearchResultsUpdating {
+class CharacterListViewController: UIViewController, UISearchResultsUpdating, UISearchBarDelegate {
     private let viewModel = CharacterListViewModel()
-    
-    func updateSearchResults(for searchController: UISearchController) {
-        guard let searchText = searchController.searchBar.text else {
-            return
-        }
-        
-        // Update the search text in the view model
-        viewModel.searchText = searchText
-        
-        // Reload the table view to display the filtered results
-        tableView.reloadData()
-    }
+    private var filteredCharacters: [RelatedTopic] = []
 
-
-
-
-
-
-    
     private let tableView = UITableView()
-    private let searchController = UISearchController(searchResultsController: nil)
-    
-    
+    private var searchController = UISearchController(searchResultsController: nil)
+
     override func viewDidLoad() {
         super.viewDidLoad()
         setupUI()
         fetchCharacterData()
         tableView.register(UITableViewCell.self, forCellReuseIdentifier: "Cell")
     }
-    
+
     private func setupUI() {
         view.backgroundColor = .black
         
-        // Create a container view for the search bar
-        let searchBarContainer = UIView()
-        searchBarContainer.translatesAutoresizingMaskIntoConstraints = false
-        view.addSubview(searchBarContainer)
-        
-        // Add the search bar to the container view
-        searchBarContainer.addSubview(searchController.searchBar)
-        
-        // Configure the search controller
+        // Create a title label
+            let titleLabel = UILabel()
+            titleLabel.text = "Character Search"
+            titleLabel.textAlignment = .center
+            titleLabel.textColor = .white
+            titleLabel.font = UIFont.boldSystemFont(ofSize: 20)
+
+
+        // Create a search bar and embed it in a search controller
+        _ = UISearchBar()
+        searchController = UISearchController(searchResultsController: nil)
         searchController.searchResultsUpdater = self
         searchController.obscuresBackgroundDuringPresentation = false
+        searchController.searchBar.delegate = self // Add the delegate
         searchController.searchBar.placeholder = "Search Characters"
-        navigationItem.searchController = searchController
-        definesPresentationContext = true
+        searchController.searchBar.showsCancelButton = false
         
+        // Create a container view for the title label and search bar
+            let headerView = UIView()
+            headerView.addSubview(titleLabel)
+            headerView.addSubview(searchController.searchBar)
+        tableView.tableHeaderView = headerView
+
+        // Set the search bar as the header view of the table view
+        tableView.tableHeaderView = searchController.searchBar
+
+        // Add the table view to the view
         tableView.translatesAutoresizingMaskIntoConstraints = false
         view.addSubview(tableView)
-        
+
+        // Set the constraints for the table view
         NSLayoutConstraint.activate([
-            // Search bar container constraints
-            searchBarContainer.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor),
-            searchBarContainer.leadingAnchor.constraint(equalTo: view.leadingAnchor),
-            searchBarContainer.trailingAnchor.constraint(equalTo: view.trailingAnchor),
-            
-            // Search bar constraints
-            searchController.searchBar.topAnchor.constraint(equalTo: searchBarContainer.topAnchor),
-            searchController.searchBar.leadingAnchor.constraint(equalTo: searchBarContainer.leadingAnchor),
-            searchController.searchBar.trailingAnchor.constraint(equalTo: searchBarContainer.trailingAnchor),
-            searchController.searchBar.bottomAnchor.constraint(equalTo: searchBarContainer.bottomAnchor),
-            
-            // Table view constraints
-            tableView.topAnchor.constraint(equalTo: searchBarContainer.bottomAnchor),
+            tableView.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor),
             tableView.leadingAnchor.constraint(equalTo: view.leadingAnchor),
             tableView.trailingAnchor.constraint(equalTo: view.trailingAnchor),
             tableView.bottomAnchor.constraint(equalTo: view.safeAreaLayoutGuide.bottomAnchor)
         ])
-        
+
+        // Set the data source and delegate of the table view
         tableView.dataSource = self
         tableView.delegate = self
         tableView.register(UITableViewCell.self, forCellReuseIdentifier: "Cell")
-        
+
         // Set the title of the view controller
         title = "The Wire Viewer"
     }
-    
+
+
     private func fetchCharacterData() {
         viewModel.fetchCharacters { [weak self] error in
             if let error = error {
@@ -96,29 +81,59 @@ class CharacterListViewController: UIViewController, UISearchResultsUpdating {
             }
         }
     }
+
+    // MARK: - UISearchResultsUpdating
+
+    func updateSearchResults(for searchController: UISearchController) {
+        guard let searchText = searchController.searchBar.text else {
+            return
+        }
+
+        filterCharacters(with: searchText)
+        tableView.reloadData()
+    }
+
+    private func filterCharacters(with searchText: String) {
+        filteredCharacters = viewModel.characters.filter { character in
+            let characterName = character.characterFullDescription.getCharacterName().uppercased()
+            let characterDescription = character.characterFullDescription.getCharacterDescription().uppercased()
+            let searchQuery = searchText.uppercased()
+
+            return characterName.contains(searchQuery) || characterDescription.contains(searchQuery)
+        }
+    }
 }
 
 // MARK: - UITableViewDataSource
 
 extension CharacterListViewController: UITableViewDataSource {
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return viewModel.getCharacterListCount()
+        return isSearchActive() ? filteredCharacters.count : viewModel.characters.count
     }
-    
+
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "Cell", for: indexPath)
-        let character = viewModel.getCharacter(at: indexPath.row)
+        let character = isSearchActive() ? filteredCharacters[indexPath.row] : viewModel.characters[indexPath.row]
         cell.textLabel?.text = character.characterFullDescription.getCharacterName()
         return cell
+    }
+
+    private func isSearchActive() -> Bool {
+        return searchController.isActive && !(searchController.searchBar.text?.isEmpty ?? true)
     }
 }
 
 // MARK: - UITableViewDelegate
-
 extension CharacterListViewController: UITableViewDelegate {
     func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
-        let relatedTopic = viewModel.getCharacter(at: indexPath.row)
-        let detailViewController = DetailViewController(characterName: relatedTopic.characterFullDescription.getCharacterName(), characterDescription: relatedTopic.characterFullDescription)
+        let character = isSearchActive() ? filteredCharacters[indexPath.row] : viewModel.characters[indexPath.row]
+        
+        // Dismiss the search controller if it is active
+        if searchController.isActive {
+            searchController.dismiss(animated: true, completion: nil)
+        }
+        
+        let detailViewController = DetailViewController(characterName: character.characterFullDescription.getCharacterName(), characterDescription: character.characterFullDescription)
         
         if let navigationController = navigationController {
             navigationController.pushViewController(detailViewController, animated: true)
@@ -128,4 +143,5 @@ extension CharacterListViewController: UITableViewDelegate {
         
         tableView.deselectRow(at: indexPath, animated: true)
     }
+    
 }
--- WireViewerKit/ViewModel/CharacterListViewModel.swift
+++ WireViewerKit/ViewModel/CharacterListViewModel.swift
@@ -8,16 +8,15 @@
 import Foundation
 
 class CharacterListViewModel {
-  
-    var characters: [RelatedTopic] = [RelatedTopic]()
-    var filteredCharacterList: [RelatedTopic] = [RelatedTopic]()
     
+    var characters: [RelatedTopic] = []
+    var filteredCharacterList: [RelatedTopic] = []
     var searchText: String = "" {
         didSet {
-            self.getFilteredCharacters(with: searchText)
+            filteredCharacterList = getFilteredCharacters(with: searchText)
         }
     }
-    
+
     func fetchCharacters(completion: @escaping (APIError?) -> Void) {
         APIHelper.shared.fetchCharacters() { [weak self] (character, error) in
             if let error = error {
@@ -31,66 +30,48 @@ class CharacterListViewModel {
             }
         }
     }
-
+    
     func getCharacterListCount() -> Int {
-        self.getFilteredCharacters(with: searchText)
-        return self.filteredCharacterList.count
+        return filteredCharacterList.count
     }
     
-    func getCharacter(at index: Int) -> RelatedTopic {
-        return self.filteredCharacterList[index]
+    func getCharacter(at index: Int, isFiltered: Bool) -> RelatedTopic {
+        let characterList = isFiltered ? filteredCharacterList : characters
+        return characterList[index]
     }
+
     
-    func getCharacterName(at index: Int) -> String {
-        return self.getCharacter(at: index).characterFullDescription.getCharacterName()
-    }
-    
-    func getCharacterDescription(at index: Int) -> String {
-        return self.getCharacter(at: index).characterFullDescription.getCharacterDescription()
-    }
-    
-    func getFilteredCharacters(with searchText: String) {
-        if self.searchText.isEmpty {
-            self.filteredCharacterList = self.characters
+    func getFilteredCharacters(with searchText: String) -> [RelatedTopic] {
+        if searchText.isEmpty {
+            return characters
         } else {
-            self.filteredCharacterList = self.characters.filter({
-                $0.characterFullDescription.getCharacterName().uppercased().contains(self.searchText.uppercased()) || $0.characterFullDescription.getCharacterDescription().uppercased().contains(self.searchText.uppercased())
+            return characters.filter({
+                $0.characterFullDescription.getCharacterName().range(of: searchText, options: .caseInsensitive) != nil ||
+                $0.characterFullDescription.getCharacterDescription().range(of: searchText, options: .caseInsensitive) != nil
             })
         }
     }
     
+    func getCharacterFromFiltered(at index: Int) -> RelatedTopic {
+        return filteredCharacterList[index]
+    }
+
 }
 
 extension String {
-   
     func getCharacterName() -> String {
-        var characterName: String = "Unknown Character Name"
-        
-        if let index = self.firstIndex(of: "-") {
-            let i = self.index(index, offsetBy: -1)
-            characterName = String(self[..<i])
+        guard let range = self.range(of: "-") else {
+            return self
         }
-        
-        return characterName
+        let name = self[..<range.lowerBound].trimmingCharacters(in: .whitespaces)
+        return String(name)
     }
     
     func getCharacterDescription() -> String {
-        var desc: String = "Unknown Character Description"
-        
-        if let index = self.firstIndex(of: "-") {
-            let i = self.index(index, offsetBy: 2)
-            desc = String(self[i...])
+        guard let range = self.range(of: "-") else {
+            return ""
         }
-        
-        return desc
+        let description = self[range.upperBound...].trimmingCharacters(in: .whitespaces)
+        return String(description)
     }
 }
-
-//extension CharacterListViewController: UITableViewDelegate {
-//    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
-//        print("Cell at index path \(indexPath) selected")
-//        let relatedTopic = viewModel.getCharacter(at: indexPath.row)
-//        let detailViewController = DetailViewController(characterDescription: relatedTopic.characterFullDescription)
-//        navigationController?.pushViewController(detailViewController, animated: true)
-//    }
-//}
